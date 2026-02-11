ceb07894718e7c1a3843d6de6607e3d67e1e24cc
Konstantin Dobrenkii <6092822@gmail.com>
2026-02-11 11:03:53 +0400

    Initialize question service repository.

---
+++ src/main/kotlin/com/kdob/piq/question/QuestionApplication.kt
@@ -0,0 +1,11 @@
+package com.kdob.piq.question
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuestionApplication
+
+fun main(args: Array<String>) {
+    runApplication<QuestionApplication>(*args)
+}
