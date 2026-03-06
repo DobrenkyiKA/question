34a6ec319e8d0296d974bd3be0da9789a5adc3f5
Konstantin Dobrenkii <6092822@gmail.com>
2026-03-06 18:40:20 +0400

    Rename repository for readability and consistency

--- src/main/kotlin/com/kdob/piq/question/infrastructure/persistence/question/JpaQuestionQueryRepository.kt
+++ src/main/kotlin/com/kdob/piq/question/infrastructure/persistence/question/JpaQuestionQueryRepository.kt
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class JpaQuestionQueryRepository(
-    private val repository: QuestionRepository
+    private val questionRepository: QuestionRepository
 ) : QuestionQueryRepository {
 
     override fun findByCriteria(
@@ -50,6 +50,6 @@ class JpaQuestionQueryRepository(
             }
         }
 
-        return repository.findAll(spec, pageable).map { it.toDomain() }
+        return questionRepository.findAll(spec, pageable).map { it.toDomain() }
     }
 }
