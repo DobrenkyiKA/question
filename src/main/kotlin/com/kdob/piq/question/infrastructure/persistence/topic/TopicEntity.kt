adc37c08b7b81bc3dfd14c8de04dd33597e6f275
Konstantin Dobrenkii <6092822@gmail.com>
2026-03-09 11:54:21 +0400

    Implement CASCADE delete of questions on topic deletion

--- src/main/kotlin/com/kdob/piq/question/infrastructure/persistence/topic/TopicEntity.kt
+++ src/main/kotlin/com/kdob/piq/question/infrastructure/persistence/topic/TopicEntity.kt
@@ -2,6 +2,7 @@ package com.kdob.piq.question.infrastructure.persistence.topic
 
 import jakarta.persistence.*
 import com.kdob.piq.question.infrastructure.persistence.BaseEntity
+import com.kdob.piq.question.infrastructure.persistence.question.QuestionEntity
 
 @Entity
 @Table(name = "topics")
@@ -22,6 +23,10 @@ class TopicEntity(
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "topics_sequence")
     @SequenceGenerator(name = "topics_sequence", sequenceName = "topics_id_sequence", allocationSize = 50)
     var id: Long? = null
+
+    @OneToMany(mappedBy = "topic", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var questions: MutableSet<QuestionEntity> = mutableSetOf()
+
     override fun getIdValue(): Long? {
         return id
     }
