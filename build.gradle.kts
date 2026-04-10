3e9d00387bf7363f9ca4b16fcb406bc04222067d
Konstantin Dobrenkii <6092822@gmail.com>
2026-04-11 00:59:22 +0400

    Rename question microservice to "content"

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 group = "com.kdob.piq"
 version = "0.0.1-SNAPSHOT"
-description = "question"
+description = "content"
 
 java {
     toolchain {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "question"
+rootProject.name = "content"
