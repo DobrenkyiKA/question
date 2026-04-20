53cbe3cabf64b4dfc4cc760f216c45f6a1954986
Konstantin Dobrenkii <6092822@gmail.com>
2026-04-21 00:22:23 +0400

    Remove Spring Devtools from all modules
    Spring Devtools dependencies and configurations were removed from all modules, as they are no longer needed. This cleanup improves consistency across the project's configuration files and ensures streamlined dependency usage. RouteConfig in the Gateway module was also refactored for better maintainability.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,17 +28,19 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.cloud:spring-cloud-starter-config:5.0.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+
     runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
     testImplementation("org.springframework.boot:spring-boot-starter-security-test")
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
